b563bd1cb02c495a5de53840e93e8b3e850c6f35
osearbhain <osearbhain@users.noreply.github.com>
2016-08-04 11:53:44 +0100

    Update flowmodgen.py

--- flowmodgen.py
+++ flowmodgen.py
@@ -13,11 +13,11 @@
 # limitations under the License.
 
 """
-A POX component to generate flow_mod messages to a switch, simulating
-busy network conditions.
+A POX component to generate flow_mod messages to a switch,
+simulating busy network conditions.
 
-This version registers SwitchGenFlowMods as a Recoco Task object, with
-its own run() method to do the recurring part.
+This version registers SendFlowMods as a Recoco Task
+object, with its own run() method to do the recurring part.
 """
 
 # Import some POX stuff
@@ -29,7 +29,7 @@ import pox.lib.util as poxutil                # Various util functions
 from pox.lib.revent import *               # Event library
 from pox.lib.recoco import *               # Multitasking library
 import weakref
-
+import time
 # Create a logger for this component
 log = core.getLogger()
 _num_switches = 0 # global variable
@@ -37,8 +37,9 @@ _num_switches = 0 # global variable
 
 class SendFlowMods (Task):
   """
-  Sends many flow modification requests to all switches that we know
-  of (Actually only doing one switch for now)
+  Sends many flow modification requests to all switches that
+  we know of (Actually assuming one switch for now, not
+  tested for multiple switches)
   """
   def __init__(self):
     Task.__init__(self)  # call our superconstructor
@@ -49,81 +50,83 @@ class SendFlowMods (Task):
 
   def start_run_loop(self, event):
     """
-    Takes a second parameter: the GoingUpEvent object (which we ignore)
+    Takes a second parameter: the GoingUpEvent object (which
+    we ignore)
     """ 
     # This causes us to be added to the scheduler's recurring Task queue
     Task.start(self) 
 
   def run(self):
     """
-    run() is the method that gets called by the scheduler to execute this task
+    run() is the method that gets called by the scheduler to
+    execute this task
     """
     
     _num_rules_installed = 0
     _current_working_set = 0
-    global _required_working_set
-    global _modsps
-    global _connref
+    global _prepop
     global _num_switches
-    lasttime = time.time()
-    currtime = lasttime
-    nummods = 0.0
+    global _barrierexpected
+    global _bcount
+    global _bps
+    global _nwb
+        
+    while core.running and (_num_switches < 1):
+      yield(Sleep(.1, False))
+
+    print('switch found, running')
+    global _starttime
+    _starttime = time.time()
+    currtime = _starttime
 
     # This is where I should send flow_mod messages to any switches that I know about
-    while core.running:
+    while core.running and (_num_rules_installed < _bcount):
       if _num_switches > 0: # then it's safe to reference _connref()
         connection = _connref()
+        #print('checking connection')
         if connection is not None: # double checking in case connection died
+          #print('connection ok')
           currtime = time.time()
-          nummods = int(currtime - lasttime) * _modsps
-          if nummods >= 1:
-            # Send 'nummods' number of flow_mod requests
-            #print('do sendmods - nummods is %s' % (nummods))
-            for i in range (1,nummods):
-              # do i need to create a new msg each time?
-              # - probably safer to
-              # note - first rule added is rule 0. It's the first
-              # deleted as well. Be careful 0-indexing doesn't
-              # lead to messed up logic later
-              msg = of.ofp_flow_mod()
-              msg.priority = 42
-              msg.cookie = _num_rules_installed
-              msg.match.dl_type = 0x800
-              msg.match.nw_dst = IPAddr("192.168.1.4")
-              #msg.match.tp_dst = 80
-              msg.hard_timeout = 600
-              msg.actions.append(of.ofp_action_output(port = 4))
-              connection.send(msg)
-              print('adding rule # {}, nmi {}, cws {}'.format(msg.cookie, _num_rules_installed, _current_working_set))
-              _current_working_set += 1
-              _num_rules_installed += 1
-              
-              if _current_working_set > _required_working_set:
-                msg = of.ofp_flow_mod()
-                msg.command = of.OFPFC_DELETE
-                msg.priority = 42
-                msg.cookie = _num_rules_installed - _current_working_set
-                msg.match.dl_type = 0x800
-                msg.match.nw_dst = IPAddr("192.168.1.4")
-                #msg.match.tp_dst = 80
-                msg.hard_timeout = 600
-                msg.actions.append(of.ofp_action_output(port = 4))
-                connection.send(msg)
-                print('removing rule # {}, nmi {}, cws {}'.format(msg.cookie, _num_rules_installed, _current_working_set))
-                _current_working_set -= 1
+          rulesneeded = int((currtime - _starttime) * float(_bps)) - _num_rules_installed
+          if ( rulesneeded >= 1 ) and ((_nwb > 0) or not _barrierexpected):
+            if rulesneeded > 1:
+              print('rulesneeded is %s' % (rulesneeded))
+            # Remove an old rule from the switch
+            msg = of.ofp_flow_mod()
+            msg.command = of.OFPFC_DELETE
+            msg.priority = 42
+            msg.match.dl_type = 0x800
+            msg.match.nw_dst = IPAddr("192.168.1.3")
+            msg.match.nw_proto=17
+            msg.match.tp_dst = _bpn + _num_rules_installed
+            msg.hard_timeout = 600
+            msg.actions.append(of.ofp_action_output(port = 3))
+            connection.send(msg)
 
+            # Add a new rule to the switch
+            msg = of.ofp_flow_mod()
+            msg.priority = 42
+            msg.match.dl_type = 0x800
+            msg.match.nw_dst = IPAddr("192.168.1.3")
+            msg.match.nw_proto=17
+            msg.match.tp_dst = _bpn + _prepop + _num_rules_installed
+            msg.hard_timeout = 600
+            msg.actions.append(of.ofp_action_output(port = 3))
+            connection.send(msg)
+            _num_rules_installed += 1
+            
+            # Send barrier request and record time sent
+            msg = of.ofp_barrier_request()
+            msg.xid = _bpn + _prepop + _num_rules_installed
+            global _outfile
+            _outfile.write(str(currtime - _starttime) + '\t' + str(msg.xid) + '\tS\n')
+            connection.send(msg)
+            _barrierexpected = True
+            #print('batch done, barrier requested at time {}'.format(time.time()))
 
-            lasttime += int(currtime - lasttime)
-      yield(Sleep(.01, False))
+      yield(Sleep(.001, False))
+    print('batches all done, ready to finish')
     yield False
-    
-  def sendmods(self, connection, nummods):
-    """
-    send however many flow_mod messages are required to maintain
-    a particular per-second rate.
-    """
-    print('in sendmods - nummods is %s' % (nummods))
-
 
 class SwitchHandler (object):
   """
@@ -145,45 +148,115 @@ class SwitchHandler (object):
     log.debug("Connection %s" % (event.connection,))
     print("in ConnectionUp")
     global _connref
-    _connref = weakref.ref(event.connection)
+    _connref = weakref.ref(event.connection) # Should really be adding to
+                                             # a list of active connections
     global _num_switches
     _num_switches += 1
-    # TODO!!!!!!!!!!!!!!!!!!!!!
-    # A new switch, so add the initial 'working set' of rules, including
-    # a default rule to send packets to host 3, then send a batch request
-    # to make sure those are all installed before setting a flag to allow
-    # the experiment to run (in 'run()' above')
+    
+    global _prepop
+    global _bpn
+    # Add default rule first
+    msg = of.ofp_flow_mod()
+    msg.priority = 42
+    msg.actions.append(of.ofp_action_output(port = 2))
+    event.connection.send(msg)
+    
+    # Prepopulate switch table with a specific number of rules
+    for i in range (0,_prepop):
+      # Creating a new msg each time, thinking that's safest.
+      msg = of.ofp_flow_mod()
+      msg.priority = 42
+      msg.match.dl_type = 0x800
+      msg.match.nw_dst = IPAddr("192.168.1.3")
+      msg.match.nw_proto=17
+      msg.match.tp_dst = _bpn + i
+      msg.hard_timeout = 600
+      msg.actions.append(of.ofp_action_output(port = 3))
+      event.connection.send(msg)
+      print('adding prepop rule # {}'.format(i))
+
+    msg = of.ofp_barrier_request()
+    msg.xid = _bpn + _prepop
+    event.connection.send(msg)
+    global _starttime
+    _starttime = time.time()
+    global _outfile
+    _outfile.write('0\t' + str(_bpn + _prepop) + '\tS\n')
+    global _barrierexpected
+    _barrierexpected = True
+    
+  def _handle_BarrierIn(self, event):
+    currtime = time.time()
+    #print('barrier received at %s' % currtime)
+    global _outfile
+    _outfile.write(str(currtime - _starttime) + '\t' + str(event.ofp.xid) + '\tR\n')
+    global _barrierexpected
+    _barrierexpected = False
     
       
-def launch (modsps = 1, setsize = 1):
-  """
-  Sets flow_mods per second and working set to values supplied as
-  arguments, or to defaults of 1 and 1 respectively.
-  
-  'modsps' is number of flow-mod messages to be generated per second
-  and 'setsize' is the size to the working set to be maintained in
-  the switch flow table.
+def launch (prepop = 1, bcount = 1, bps = 1, bpn = 1, nwb = 1):
+  """  
+  'prepop' is the number of rules with which a switch table should be
+  prepopulated. 'bcount' is the number of batches of rule updates to be
+  applied, each batch having one rule deletion and one rule addition.
+  'bps' is number of batches to be generated per second. 'bpn' is base
+  port number - in order to keep rules unique, the destination port
+  number is incremented for each rule added. 'nwb' is a flag to direct
+  us to not wait for barrier replies to come back before sending the
+  next batch. 
   
   Call this component as, e.g.:
-  ./pox.py flowmodgen --modsps=100 --setsize=50
+  ./pox.py flowmodgen --prepop=300 --bcount=500 --bps=50 --bpn=44000 --nwb=1
   """
 
-  # Arguments passed from the commandline are ordinarily always
-  # strings, so we need to validate and convert modsps and setsize.
+  # Arguments passed from the commandline are ordinarily string type,
+  # so we need to validate and convert to integer.
 
   try:
-    global _modsps
-    _modsps = int(str(modsps), 10)
-    assert _modsps >= 0
+    global _prepop
+    _prepop = int(str(prepop), 10)
+    assert _prepop >= 0
   except:
-    raise RuntimeError("Expected modsps to be a number")
+    raise RuntimeError("Expected prepop to be a number")
 
   try:
-    global _required_working_set
-    _required_working_set = int(str(setsize), 10)
-    assert _required_working_set >= 0
+    global _bcount
+    _bcount = int(str(bcount), 10)
+    assert _bcount >= 0
   except:
-    raise RuntimeError("Expected setsize to be a number")
+    raise RuntimeError("Expected bcount to be a number")
+
+  try:
+    global _bps
+    _bps = int(str(bps), 10)
+    assert _bps >= 0
+  except:
+    raise RuntimeError("Expected bps to be a number")
+
+  try:
+    global _bpn
+    _bpn = int(str(bpn), 10)
+    assert _bpn >= 0
+  except:
+    raise RuntimeError("Expected bps to be a number")
+
+  try:
+    global _nwb
+    _nwb = int(str(nwb), 10)
+    assert _nwb >= 0
+  except:
+    raise RuntimeError("Expected nwb to be a number")
+
+  global _num_switches
+  _num_switches = 0
+ 
+  global _outfile
+  filename = "fmgresults-" + time.strftime("%Y-%m-%d-%H%M%S" + ".txt")
+  _outfile = open(filename, 'w')
+  _outfile.write('Parameters:\nprepop\tbcount\tbps\tbpn\tnwb\n')
+  _outfile.write('{}\t{}\t{}\t{}\t{}\n\n'.format(_prepop, _bcount, _bps, _bpn, _nwb))
+  _outfile.write('Time\tXid\tSent/Received\n')
+  
 
   core.registerNew(SendFlowMods)
   core.registerNew(SwitchHandler)
